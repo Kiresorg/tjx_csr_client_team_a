5873c57334ba6801b2b11a564ff216ced770a46d
Pat Kelley <patrick_kelley@tjx.com>
2021-03-16 16:30:49 -0400

    js works with login button

--- js/login.js
+++ js/login.js
@@ -6,7 +6,10 @@ document.getElementById("login_button").addEventListener("click", loginEvent);
 //put the user's login info in an object and send it (to the console for right now)
 function loginEvent(){
     let loginInfo = {}
-    loginInfo.email = document.getElementById("email_input").innerText;
-    loginInfo.password = document.getElementById("password_input").innerText;
+    loginInfo.email = document.getElementById("email_input").value;
+    loginInfo.password = document.getElementById("password_input").value;
     console.log(loginInfo);
+    document.getElementById("email_input").value = "";
+    document.getElementById("password_input").value ="";
+
 }
