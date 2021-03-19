d5d0ce3b332de3ef469a8dca400e55d98f58e967
Cameron Sanborn <cam02299@tjx.com>
2021-03-19 11:19:28 -0400

    Finished implementing sleect quantity and Add to Order button

--- js/productinfo.js
+++ js/productinfo.js
@@ -1,3 +1,7 @@
+function loadViewProducts() {
+  window.location = 'viewProducts.html';
+}
+
 $( document ).ready(function() {
     getProductDescription(Number(localStorage.getItem("productId")));
 });
@@ -9,15 +13,41 @@ async function getProductDescription(id) {
     var html = '';
     
     html += '<div class="box">';
-    html += '<a class="title has-text-black">' + data.name + '</a>';
-    html += '<div class="content"> <br>';
-    html += '<p class="subtitle has-text-black"> SKU: ' + data.sku + '</p>';
-    html += '<p class="subtitle has-text-black">Product Description: ' + data.description + '</p>';
-    html += '<p class="text1 has-text-black">Quantity Available: ' + data.quantity + '</p>';
-    html += '<p class="text1">Price: ' + data.unit_price + '</p>';
+      html += '<a class="title has-text-black">' + data.name + '</a>';
+      html += '<div class="content"> <br>';
+        html += '<p class="subtitle has-text-black"> SKU: ' + data.sku + '</p>';
+        html += '<p class="subtitle has-text-black">Product Description: ' + data.description + '</p>';
+        html += '<p class="text1 has-text-black">Quantity Available: ' + data.quantity + '</p>';
+        html += '<p class="text1">Price: ' + data.unit_price + '</p>';
+      html += '</div>';
+    html += '<div>';
+      html += '<select name="quantityMenu" id="quantity">';
+      html += '<option value="selectQuantity">Select Quantity</option>';
+
+    for (var i = 1; i < 11; i++) {
+        html += '<option value="' + i + '">' + i + '</option>'; 
+    }
+
+      html += '</select>';
+      html += '<button class="button is-success" onclick=' + `loadViewProducts()` + '>Add to Order</button>';
+      html += '<hr class="dropdown-divider">';
     html += '</div>';
+
+    $('#productContainer').html(html);
+};
+
+
+/* html += '<div class="box">';
+      html += '<a class="title has-text-black">' + data.name + '</a>';
+      html += '<div class="content"> <br>';
+        html += '<p class="subtitle has-text-black"> SKU: ' + data.sku + '</p>';
+        html += '<p class="subtitle has-text-black">Product Description: ' + data.description + '</p>';
+        html += '<p class="text1 has-text-black">Quantity Available: ' + data.quantity + '</p>';
+        html += '<p class="text1">Price: ' + data.unit_price + '</p>';
+      html += '</div>';
     html += '<div class="dropdown is-hoverable">';
     html += '<div class="dropdown-trigger">';
+    html += '<div>';
     html += '<button class="button" aria-haspopup="true" aria-controls="dropdown-menu">';
     html += '<span>Select Quantity</span>';
     html += '<span class="icon is-small">';
@@ -27,44 +57,17 @@ async function getProductDescription(id) {
     html += '</div>';
     html += '<div class="dropdown-menu" id="dropdown-menu" role="menu">';
     html += '<div class="dropdown-content">';
+    
+      html += '<select name="quantityMenu" id="quantity">';
+      html += '<option value="selectQuantity">Select Quantity</option>';
 
-    for (var i = 0; i < 11; i++) {
-        html += '<a href="#" class="dropdown-item">' + i + '</a>';
+    for (var i = 1; i < 11; i++) {
+        //html += '<a href="#" class="dropdown-item">' + i + '</a>';
+        html += '<option value="' + i + '">' + i + '</option>'; 
     }
 
-    html += '<hr class="dropdown-divider">';
-    html += '<a href="#" class="dropdown-item">Other Amount</a>';
-    html += '</div></div>';
-    html += '<button class="button is-success" href="">Add to Order</button>';
-
-    $('#productContainer').html(html);
-};
-
-
-/* <div class="dropdown is-hoverable">
-                      <div class="dropdown-trigger">
-                        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                          <span>Select Quantity</span>
-                          <span class="icon is-small">
-                            <i class="fas fa-angle-down" aria-hidden="true"></i>
-                          </span>
-                        </button>
-                      </div> 
-                      
-                      <div class="dropdown-menu" id="dropdown-menu" role="menu">
-                        <div class="dropdown-content">
-                          <a href="#" class="dropdown-item">1</a>
-                          <a class="dropdown-item">2</a>
-                          <a href="#" class="dropdown-item">3</a>
-                          <a href="#" class="dropdown-item">4</a>
-                          <a href="#" class="dropdown-item">5</a>
-                          <a href="#" class="dropdown-item">6</a>
-                          <a href="#" class="dropdown-item">7</a>
-                          <a href="#" class="dropdown-item">8</a>
-                          <a href="#" class="dropdown-item">9</a>
-                          <a href="#" class="dropdown-item">10</a>
-                          <hr class="dropdown-divider">
-                          <a href="#" class="dropdown-item">Other Amount</a>
-                        </div>
-                      </div>
-                      <button class="button is-success">Add to Order</button> */
+      html += '</select>';
+      html += '<button class="button is-success" onclick=' + `loadViewProducts()` + '>Add to Order</button>';
+      html += '<hr class="dropdown-divider">';
+    //html += '<a href="#" class="dropdown-item">Other Amount</a>';
+    html += '</div>'; */
